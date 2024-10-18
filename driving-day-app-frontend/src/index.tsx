de0edddded884686c49d3294c2236e1555edd7ea
ethany202 <ethan.ye0312@gmail.com>
2024-10-18 15:45:29 -0400

    feat: Imported the react-router-dom package to handle static routing

--- driving-day-app-frontend/src/index.tsx
+++ driving-day-app-frontend/src/index.tsx
@@ -3,14 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+/**
+ * Creates an object that handles routing for all webpages:
+ *    EX: /home --> presents the <App/> page
+ */
+const router = createBrowserRouter([
+  {
+    path: "/",
+
+    // TODO: Replace all elements with the desired webpage element
+    element: <App />,
+    children: [
+      {
+        path: "/home",
+        element: <App />
+      },
+      {
+        path: "/run-summary",
+        element: <App />
+      },
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
 
 // If you want to start measuring performance in your app, pass a function
