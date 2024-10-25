e64144a5332913033c912eb07d6373b34b24afd7
Yubo Jing <jybjybjing@yubodeair.mynetworksettings.com>
2024-10-25 12:06:16 -0400

    update component folder

--- driving-day-app-frontend/src/components/navbar-components/Navbar.tsx
+++ driving-day-app-frontend/src/components/navbar-components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/image.png';
+import logo from '../../assets/image.png';
 
 const Navbar = () => {
   return (
