ef94f07d2e96c6e9a04e8b81c42b82db210e125b
ethany202 <ethan.ye0312@gmail.com>
2024-11-06 16:30:45 -0500

    feat: Added code for necessary POST and GET requests in api.tsx

--- driving-day-app-frontend/src/api/api.tsx
+++ driving-day-app-frontend/src/api/api.tsx
@@ -3,21 +3,32 @@ import { collection, getDocs, addDoc } from 'firebase/firestore';
 import { db } from './firebaseConfig';
 
 const api = axios.create({
-    baseURL: `${process.env.REACT_APP_BACKEND_URL}:${process.env.REACT_APP_BACKEND_PORT}/api/`,
+    baseURL: `${process.env.REACT_APP_BACKEND_URL}/api/`,
     timeout: 10000,
 });
 
-export const postUserRegistration = async (userData: { payload: string; }) => {
+// TODO: Configure POST request to use API token to obtain content/register
+/**
+ * 
+ * @param content: JSON data, consisting of the JSON content that should be sent to the backend upon making a POST request
+ * @param path: string, corresponding to the path for the POST request
+ * @returns: JSON content, representing the result of the POST request
+ */
+export const postRequest = async (content: any, path: string) => {
     try {
-        console.log("Testing User Registration (POST Request)")
-        const response = await api.post('user-registration/', userData,);
-        return response.data;
-    } catch (error) {
-        console.error(error);
-        throw error;
+        const response = await api.post(path, content);
+        return response
     }
-};
+    catch (err) {
+        console.error(err);
+        return {}
+    }
+}
 
+export const postUserRegistration = async (userData: { payload: string; }) => {
+    const path = 'user-registration/';
+    return (await postRequest(userData, path))
+};
 
 export const postDriverProfile = async (profileData: {
     firstName: string;
@@ -28,10 +39,51 @@ export const postDriverProfile = async (profileData: {
 }) => {
     try {
         const docRef = await addDoc(collection(db, 'driver-profiles'), profileData);
-        console.log('New Driver Profile added with ID:', docRef.id);
         return { id: docRef.id, ...profileData };
     } catch (error) {
         console.error('Error adding driver profile:', error);
-        throw error;
+        return {}
     }
 };
+
+/**
+ * 
+ * @param content: JSON data, consisting of the JSON content that should be sent to the backend upon making a GET request
+ * @param path: string, corresponding to the path for the GET request
+ * @returns: JSON content, representing the result of the GET request
+ */
+export const getRequest = async (content: any, path: string) => {
+    try {
+        const response = await api.get(path, content);
+        return response;
+    }
+    catch (err) {
+        console.error(err);
+        return {}
+    }
+}
+
+export const getDriverData = async (driverFilter: {
+    height: number,
+    weight: number
+}) => {
+    const path = 'driver-data'
+    return (await getRequest(driverFilter, path))
+}
+
+// TODO: Implement pagination/filtering
+export const getRunData = async (runFilter: {
+    date: Date,
+    driverID: number
+}) => {
+    const path = 'run-data'
+    return (await getRequest(runFilter, path))
+}
+
+// Pull data specifically for a given RUN
+export const getRunByID = async (runFilter: {
+    runFilter: number
+}) => {
+    const path = 'specific-run'
+    return (await getRequest(runFilter, path))
+}
