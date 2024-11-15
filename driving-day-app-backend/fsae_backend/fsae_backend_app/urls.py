d544dae60e51f3037ff9380dee6b07040948f415
Austin Liu <austin.liu05@yahoo.com>
2024-11-15 15:43:10 -0500

    Fixed bug

--- driving-day-app-backend/fsae_backend/fsae_backend_app/urls.py
+++ driving-day-app-backend/fsae_backend/fsae_backend_app/urls.py
@@ -3,7 +3,6 @@ from .views import user_registration, upload_ld
 from .views import driver_profiles
 
 urlpatterns = [
-    path('user-registration/', user_registration, name='user-registration'),
     path('upload-data/', upload_ld, name='upload-data'),
     path('driver-profiles/', driver_profiles, name='driver-profiles'),
 ]
