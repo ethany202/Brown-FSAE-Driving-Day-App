9ce90a3f5db7d430ed52214ea643d1febc18896f
ethany202 <ethan.ye0312@gmail.com>
2024-12-06 18:01:17 -0500

    feat: Working on Upload Files page

--- driving-day-app-frontend/src/components/upload-components/UploadComponent.tsx
+++ driving-day-app-frontend/src/components/upload-components/UploadComponent.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
-import * as FilePond from 'filepond';
+import React, { useState } from 'react';
+import { FilePond, registerPlugin } from 'react-filepond';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import 'filepond/dist/filepond.min.css';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 
-export default function UploadComponent({ }) {
+interface UploadComponentProps {
+    maxFiles: number,
+    fileDesc: string
+}
 
-    const pond = FilePond.create({
-        multiple: true,
-        name: 'filepond'
-    })
+export default function UploadComponent({ maxFiles, fileDesc }: UploadComponentProps) {
 
+    const [uploadedFiles, setUploadedFiles] = useState<any>([])
 
+    registerPlugin(FilePondPluginImagePreview);
+
+    return (
+        <div className="w-3/4 py-4 justify-center">
+            <div className="text-center p-2">
+                <p>{fileDesc}</p>
+            </div>
+            <FilePond
+                allowMultiple={true}
+                onupdatefiles={setUploadedFiles}
+                maxFiles={maxFiles}
+                name="files"
+                imagePreviewHeight={192}
+                imagePreviewMaxHeight={228}
+                acceptedFileTypes={["ld", "csv"]} />
+        </div>
+    )
 }
--- driving-day-app-frontend/src/pages/upload-page/UploadFiles.tsx
+++ driving-day-app-frontend/src/pages/upload-page/UploadFiles.tsx
@@ -1,20 +1,24 @@
 import React, { useState, useEffect } from "react";
+import UploadComponent from "../../components/upload-components/UploadComponent";
 
 export default function UploadFiles({ }) {
 
-
-
     return (
-        <div className="upload-files-wrapper">
-
-            <div className="upload-header">
-                <h2>Upload Data</h2>
+        <div className="page-content-main">
+            <div className="flex justify-center py-16 textxs/[24px]">
+                <h1>Upload Data</h1>
             </div>
 
             { /** File input system */}
-            <input type="file">
+            <div className="flex justify-center flex-col items-center">
+                <UploadComponent
+                    fileDesc="Upload a CSV or LD file"
+                    maxFiles={1} />
 
-            </input>
+                <UploadComponent
+                    fileDesc="Upload Images or Videos"
+                    maxFiles={5} />
+            </div>
         </div>
     )
 }
