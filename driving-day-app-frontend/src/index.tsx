517db2373a9035896bdbcad8ec80134c0c803cea
ethany202 <ethan.ye0312@gmail.com>
2024-12-06 15:24:21 -0500

    feat: Created a new page to be able to upload LD and CSV files

--- driving-day-app-frontend/src/index.tsx
+++ driving-day-app-frontend/src/index.tsx
@@ -7,6 +7,7 @@ import RunsSummary from './pages/run-summary/RunSummary';
 import RunDetail from './pages/run-page/RunDetail';
 import Layout from './pages/layout/Layout';
 import DriversPage from './pages/drivers-page/DriversPage';
+import UploadFiles from './pages/upload-page/UploadFiles';
 
 /**
  * Creates an object that handles routing for all webpages:
@@ -38,6 +39,10 @@ const router = createBrowserRouter([
       {
         path: "/my-account",
         element: <App />
+      },
+      {
+        path: "/upload-files",
+        element: <UploadFiles />
       }
     ]
   }
---
+++ driving-day-app-frontend/src/pages/upload-page/UploadFiles.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+
+export default function UploadFiles({ }) {
+    return (
+        <div className="upload-files-wrapper">
+
+
+            { /** File input system */}
+            <input type="file">
+
+            </input>
+        </div>
+    )
+}
