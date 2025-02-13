dfb5a72ff3443bcfec80639ca66cf0279bb44661
Yubo Jing <yubo_jing@brown.edu>
2025-02-13 16:57:08 -0500

    oauth

---
+++ driving-day-app-frontend/src/pages/account-page/MyAccount.tsx
@@ -0,0 +1,11 @@
+function MyAccount() {
+  return (
+    <div className="page-content-main">
+        <div className="flex justify-center py-16">
+            <h1>My Account</h1>
+        </div>
+    </div>
+  );
+}
+
+export default MyAccount;
